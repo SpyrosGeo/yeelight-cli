1088ce2908d797600ac18521925b2777bd8f528d
thatguy <xarodasimba@gmail.com>
2019-12-24 12:29:10 +0200

    added universal directory for moving it to other devices

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 var inquirer = require('inquirer');
+var path = require('path')
 var finalIp='test';
-
 
 export function cli(args){
   if(args[2] == null){
@@ -15,20 +15,20 @@ export function cli(args){
   
      if(cmd === "on"|| cmd ==="off"|| cmd ==="disco"||cmd ==="warm"){
       //added Capitalization of the first letter to work with the script
-      exec(`/home/thatguy/My-repos/ylight/scripts/yeelight-scene.sh 0 ${cmd.charAt(0).toUpperCase()+cmd.slice(1)}`);
+      exec(`${__dirname}/../scripts/yeelight-scene.sh 0 ${cmd.charAt(0).toUpperCase()+cmd.slice(1)}`);
       console.log(`lights ${cmd}!`);
-    }else if(cmd === "default"|| cmd ==="6500"){
-      exec('/home/thatguy/My-repos/ylight/scripts/yeelight-scene.sh 0 6500');
-      exec('/home/thatguy/My-repos/ylight/scripts/yeelight-brightness.sh 0 100');
-	console.log("setting to default!");
+    }else if(cmd === "default"){
+      exec(`${__dirname}/../scripts/yeelight-scene.sh 0 6500`);
+      exec(`${__dirname}/../scripts/yeelight-brightness.sh 0 100`);
+	  console.log("setting to default!");
     }else if(cmd === "50"||cmd==="100") {
-      exec(`/home/thatguy/My-repos/ylight/scripts/yeelight-brightness.sh 0 ${cmd}`);
+      exec(`${__dirname}/../scripts/yeelight-brightness.sh 0 ${cmd}`);
     }else if(cmd ==="blue"){
-      exec(`/home/thatguy/My-repos/ylight/scripts/yeelight-rgb.sh 0 0,0,255`);
+      exec(`${__dirname}/../scripts/yeelight-rgb.sh 0 0,0,255`);
     }else if(cmd ==="red"){
-      exec(`/home/thatguy/My-repos/ylight/scripts/yeelight-rgb.sh 0 255,0,0`);
+      exec(`${__dirname}/../scripts/yeelight-rgb.sh 0 255,0,0`);
     }else if(cmd ==="green"){
-      exec(`/home/thatguy/My-repos/ylight/scripts/yeelight-rgb.sh 0 0,255,0`);
+      exec(`${__dirname}/../scripts/yeelight-rgb.sh 0 0,255,0`);
     }else if (cmd !== "on" || cmd !== "off" || cmd !== "disco" || cmd !== "warm" || cmd !== "default" || cmd !== "help") {
       console.log("Not valid command! run 'ylight' for commands");
     }
