29a83ea8f4a20d9d76e8ad67ab25388109840ef5
thatguy <xarodasimba@gmail.com>
2019-12-25 19:04:12 +0200

    added bulb discovery using the yeeligt-platform package. I also modified the discovery.js of the package so that the app stops from listening all the time

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,35 @@
+'use strict'
 var exec = require('child_process').exec;
 let {PythonShell} = require('python-shell')
 const fs = require('fs');
 var path = require('path')
+let ip ='test'
+const YeeDiscovery = require('yeelight-platform').Discovery
+const discoveryService = new YeeDiscovery()
 
+function discover(){
+discoveryService.on('started', () => {
+  // console.log('** Discovery Started **')
+})
 
+discoveryService.on('didDiscoverDevice', (device) => {
+  //  ip = device.host
+  let ip = device.host;
+     fs.writeFile(`${__dirname}/../scripts/yeelight-ips`,ip,(err)=>{
+         if(err) throw err
+         console.log('added IP :',ip)
+       })
+})
+
+discoveryService.listen()
+}
 export function cli(args){
   if(args[2] == null){
     console.log("Use the following commands: init||on||off||disco||warm||default|red|green|blue");
     console.log("You can also use commands to set Brightness with number pernentage without the % ");
   }else {
   const cmd = args[2].toLowerCase();
-  const ip = args[3];
+  // const ip = args[3];
   // const fnArg = (args.splice(2));
 
   const rgb = args[3];
@@ -33,10 +52,7 @@ export function cli(args){
     }else if(cmd ==="green"){
       exec(`${__dirname}/../scripts/yeelight-rgb.sh 0 0,255,0`);
     }else if(cmd === 'init'){
-       fs.writeFile(`${__dirname}/../scripts/yeelight-ips`,ip,(err)=>{
-         if(err) throw err
-         console.log('added IP :',ip)
-       })
+    discover();
     }else if (cmd !== "on" || cmd !== "off" || cmd !== "disco" || cmd !== "warm" || cmd !== "default" || cmd !== "help") {
       console.log("Not valid command! run 'ylight' for commands");
     }
