98d758804b2e7de16a70d63534dec8fa9e20d40a
SpyrosGeo <xarodasimba@gmail.com>
2020-02-23 17:40:58 +0200

    added another mode and fixed the percentage of brightness

--- src/cli.js
+++ src/cli.js
@@ -1,64 +1,61 @@
 'use strict'
 var exec = require('child_process').exec;
-let {PythonShell} = require('python-shell')
 const fs = require('fs');
 var path = require('path')
-let ip ='test'
+let ip = 'test'
 const YeeDiscovery = require('yeelight-platform').Discovery
 const discoveryService = new YeeDiscovery()
 
-function discover(){
-discoveryService.on('started', () => {
-  // console.log('** Discovery Started **')
-})
+function discover() {
+  discoveryService.on('started', () => {
+    // console.log('** Discovery Started **')
+  })
 
-discoveryService.on('didDiscoverDevice', (device) => {
-  //  ip = device.host
-  let ip = device.host;
-     fs.writeFile(`${__dirname}/../scripts/yeelight-ips`,ip,(err)=>{
-         if(err) throw err
-         console.log('added IP :',ip)
-       })
-})
+  discoveryService.on('didDiscoverDevice', (device) => {
+    //  ip = device.host
+    let ip = device.host;
+    fs.writeFile(`${__dirname}/../scripts/yeelight-ips`, ip, (err) => {
+      if (err) throw err
+      console.log('added IP :', ip)
+    })
+  })
 
-discoveryService.listen()
+  discoveryService.listen()
 }
-export function cli(args){
-  if(args[2] == null){
+export function cli(args) {
+  if (args[2] == null) {
     console.log("Use the following commands: init||on||off||disco||warm||default|red|green|blue");
     console.log("You can also use commands to set Brightness with number pernentage without the % ");
-  }else {
-  const cmd = args[2].toLowerCase();
-  // const ip = args[3];
-  // const fnArg = (args.splice(2));
+  } else {
+    const cmd = args[2].toLowerCase();
 
-  const rgb = args[3];
- 
-  
-     if(cmd === "on"|| cmd ==="off"|| cmd ==="disco"||cmd ==="warm"){
+    const rgb = args[3];
+
+
+    if (cmd === "on" || cmd === "off" || cmd === "disco" || cmd === "warm" || cmd === "sunrise") {
       //added Capitalization of the first letter to work with the script
-      exec(`${__dirname}/../scripts/yeelight-scene.sh 0 ${cmd.charAt(0).toUpperCase()+cmd.slice(1)}`);
+      exec(`${__dirname}/../scripts/yeelight-scene.sh 0 ${cmd.charAt(0).toUpperCase() + cmd.slice(1)}`);
       console.log(`lights ${cmd}!`);
-    }else if(cmd === "default"){
+    } else if (cmd === "default") {
       exec(`${__dirname}/../scripts/yeelight-scene.sh 0 6500`);
       exec(`${__dirname}/../scripts/yeelight-brightness.sh 0 100`);
-	  console.log("setting to default!");
-    }else if(cmd === "50"||cmd==="100") {
+      console.log("setting to default!");
+    } else if (Number(cmd) >= 10 && Number(cmd) <= 100) {
       exec(`${__dirname}/../scripts/yeelight-brightness.sh 0 ${cmd}`);
-    }else if(cmd ==="blue"){
+    } else if (cmd === "blue") {
       exec(`${__dirname}/../scripts/yeelight-rgb.sh 0 0,0,255`);
-    }else if(cmd ==="red"){
+    } else if (cmd === "red") {
       exec(`${__dirname}/../scripts/yeelight-rgb.sh 0 255,0,0`);
-    }else if(cmd ==="green"){
+    } else if (cmd === "green") {
       exec(`${__dirname}/../scripts/yeelight-rgb.sh 0 0,255,0`);
-    }else if(cmd === 'init'){
-    discover();
-    }else if (cmd !== "on" || cmd !== "off" || cmd !== "disco" || cmd !== "warm" || cmd !== "default" || cmd !== "help") {
+    } else if (cmd === 'init') {
+      discover();
+    } else if (cmd !== "on" || cmd !== "off" || cmd !== "disco" || cmd !== "warm" || cmd !== "default" || cmd !== "help") {
       console.log("Not valid command! run 'ylight' for commands");
     }
-    
-    
-}
+
+
+  }
 
 }
 
